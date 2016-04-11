1073cffb6fa9d96b66b331caa92474af193c3ca5
Natan-Artini <natanartini@hotmail.com>
2016-04-11 09:20:52 -0300

    Update Trabalho 4.v

--- Trabalho 4.v
+++ Trabalho 4.v
@@ -1,34 +1,60 @@
 module calculo(
-	input [10:0] p1x,
-	input [10:0] p1y,
-	input [10:0] p2x,
-	input [10:0] p2y,
-	input [10:0] p3x,
-	input [10:0] p3y,
-	input CLOCK,
-	output saida
+  input [10:0] p1x,
+  input [10:0] p1y,
+  input [10:0] p2x,
+  input [10:0] p2y,
+  input [10:0] p3x,
+  input [10:0] p3y,
+  output saida
 );
 
-	reg signed [23:0] mult1;
-	reg signed [23:0] mult2;
-	
-	always @(posedge CLOCK) begin
-		mult1 <= (p1x - p3x) * (p2y - p3y);
-		mult2 <= (p2x - p3x) * (p1y - p3y);
-	end
-	assign saida = mult1 < mult2;
-		
+  wire signed [11:0] t1;
+  wire signed [11:0] t2;
+  wire signed [11:0] t3;
+  wire signed [11:0] t4;
+  wire signed [23:0] m1;
+  wire signed [23:0] m2;
+
+  assign t1 = p1x - p3x;
+  assign t2 = p2y - p3y;
+  assign t3 = p2x - p3x;
+  assign t4 = p1y - p3y;
+
+  assign m1 = t1 * t2;
+  assign m2 = t3 * t4;
+  
+  assign saida = m1 < m2;
+
 endmodule
 
-module Trabalho(
-	input [10:0]x,
-	input [10:0]y,
-	input CLOCK,
-	input reset,
-	output saida,
-	output disponivel
+module triangle(
+  input [10:0] ptx,
+  input [10:0] pty,
+  input [10:0] p1x,
+  input [10:0] p1y,
+  input [10:0] p2x,
+  input [10:0] p2y,
+  input [10:0] p3x,
+  input [10:0] p3y,
+  output result
 );
 
+
+wire s1, s2, s3; 
+
+calculo S1(ptx, pty, p1x, p1y, p2x, p2y, s1);
+calculo S2(ptx, pty, p2x, p2y, p3x, p3y, s2);
+calculo S3(ptx, pty, p3x, p3y, p1x, p1y, s3);
+
+
+assign result = s1 == s2 & s2 == s3;
+
+endmodule
+
+module teste;
+	
+	integer escrever;
+	
 	reg [10:0]x1;
 	reg [10:0]x2;
 	reg [10:0]x3;
@@ -37,54 +63,38 @@ module Trabalho(
 	reg [10:0]y3;
 	reg [10:0]px;
 	reg [10:0]py;
-	reg [3:0]state;
-	reg controle;
-	reg saida;
-	wire result;
-	wire valor1;
-	wire valor2;
-	wire valor3;
-
-	calculo A(px, py, x1, y1, x2, y2,CLOCK,valor1);
-	calculo B(px, py, x2, y2, x3, y3,CLOCK,valor2);
-	calculo C(px, py, x3, y3, x1, y1,CLOCK,valor3);
+	reg CLOCK;
+	reg reset;
 	
-	assign result = valor1 == valor2 & valor2 == valor3;
+	triangle A(px,py,x1,y1,x2,y2,x3,y3,saida);
 	
-	assign disponivel = controle;
-	
-	always @(posedge CLOCK) begin
-		if(reset) begin
-			state <=0;
-		end else begin
-			case (state)
-				0: begin
-					x1 <= x;
-					y1 <= y;
-					controle <=0;
-					state <=1;
-				end
-				1: begin
-					x2 <= x;
-					y2 <= y;
-					state <=2;
-				end
-				2: begin
-					x3 <= x;
-					y3 <= y;
-					state <=3;
-				end
-				3: begin
-					px <= x;
-					py <= y;
-					state <=4;
-				end
-				4: begin
-					saida <= result;
-					controle <=1;
-					state <=0;
-				end
-			endcase
-		end
+	initial begin
+		escrever = $fopen("resultado_V.txt", "w");
+		$dumpvars(0, A);
+			#0;
+				x1 <=0;
+				y1 <=0;
+				x2 <=10;
+				y2 <=0;
+				x3 <=0;
+				y3 <=10;
+				px <=3;
+				py <=3;
+			#5;
+				$fdisplay(escrever, "%d", saida);
+			#6;
+				x1 <=15;
+				y1 <=15;
+				x2 <=30;
+				y2 <=0;
+				x3 <=15;
+				y3 <=0;
+				px <=3;
+				py <=3;
+			#9;
+				$fdisplay(escrever, "%d", saida);
+			#10;
+		$finish;
 	end
+
 endmodule
